94e39caa9125386261dfeffb9c51e7f43a64c859
melk0r64 <106148952+melk0r64@users.noreply.github.com>
2022-05-24 11:11:35 +0200

    Create Resampler_FLAC.sh

---
+++ Resampler_FLAC.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SourceDirectory=$1
+DestinedDirectory=$2
+TargetCompression=5
+BitRate=176400
+
+mkdir -p "$DestinedDirectory"
+
+find "$SourceDirectory" -type f -print0 | while read -d $'\0' FoundFile
+do
+  echo "Processing" $FoundFile
+
+  OutputFile=$DestinedDirectory${FoundFile#"$SourceDirectory"}
+  OutputDirectory=$(dirname "$OutputFile")
+
+  if [[ "$FoundFile" == *.flac ]]
+  then
+    mkdir -p "$OutputDirectory"/spectrals/
+    OutputFile=${OutputFile%.flac}.flac
+    OutputBasename=$(basename "$OutputFile")
+    sox "$FoundFile" -C $TargetCompression -S -G -V3 -b 24 "$OutputFile" lowpass -2 21000 > lowpass.plt rate -v -s -b 90 $BitRate
+    sox "$OutputFile" -n spectrogram -o "$OutputDirectory/spectrals/${OutputBasename%.flac}.jpg"
+  else
+    mkdir -p "$OutputDirectory"
+    cp "$FoundFile" "$OutputFile"
+  fi
+
+  echo "Saved as" $OutputFile
+done
