799a94620b5e7b910c9a1fe7eee66c031653e0fd
melk0r64 <106148952+melk0r64@users.noreply.github.com>
2022-05-26 07:06:28 +0200

    Create Resampler_TEST.sh
    git clone https://github.com/rhgg2/sox.git
    cd sox
    autoreconf -i
    ./configure
    make
    make install

---
+++ Resampler_TEST.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+SourceDirectory=$1
+DestinedDirectory=$2
+TargetCompression=5
+BitRate=192000
+TAPS=64000000
+
+mkdir -p "$DestinedDirectory"
+
+find "$SourceDirectory" -type f -print0 | while read -d $'\0' FoundFile
+do
+  echo "Processing" $FoundFile
+
+  OutputFile=$DestinedDirectory${FoundFile#"$SourceDirectory"}
+  OutputDirectory=$(dirname "$OutputFile")
+
+  if [[ "$FoundFile" == *.flac ]]
+  then
+    mkdir -p "$OutputDirectory"/spectrals/
+    OutputFile=${OutputFile%.flac}.flac
+    OutputBasename=$(basename "$OutputFile")
+#    sox "$FoundFile" -C $TargetCompression -S -G -V3 -b 24 "$OutputFile" lowpass -2 21000 rate -v -s -b 90 $BitRate
+#    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" upsample 4 sinc -22050 -n $TAPS -L -b 0 vol 2
+#    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" upsample 4 sinc -22050 -n $TAPS -L -b 12.5 vol 2
+#    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" upsample 4 sinc -a80 -L -21k -n $TAPS
+#    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" lowpass -2 21000 upsample 4 sinc -22050 -n $TAPS -b 0 vol 3
+#    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" upsample 2 sinc -a80 -L -21k -n $TAPS
+#    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" upsample 4 sinc -a80 -L -21k -n $TAPS
+#    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" sinc -a80 -I -21.5k -t8000
+#    sox "$OutputFile" -n spectrogram -o "$OutputDirectory/spectrals/${OutputBasename%.flac}.jpg"
+    sox "$OutputFile" -n spectrogram -o "$OutputDirectory/spectrals/${OutputBasename%.flac}.jpg"
+  else
+    mkdir -p "$OutputDirectory"
+    cp "$FoundFile" "$OutputFile"
+  fi
+
+  echo "Saved as" $OutputFile
+done
