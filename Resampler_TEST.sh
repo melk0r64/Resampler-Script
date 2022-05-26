b19afc7e15214d62169d7b2ff0a908473d638a69
melk0r64 <106148952+melk0r64@users.noreply.github.com>
2022-05-26 16:35:25 +0200

    Update Resampler_TEST.sh

--- Resampler_TEST.sh
+++ Resampler_TEST.sh
@@ -20,15 +20,23 @@ do
     mkdir -p "$OutputDirectory"/spectrals/
     OutputFile=${OutputFile%.flac}.flac
     OutputBasename=$(basename "$OutputFile")
+#    sox "$FoundFile" -C $TargetCompression -S -G -V6 -b 24 "$OutputFile" rate -u -b 99.9 -I $BitRate
+#    sox "$FoundFile" -C $TargetCompression -S -G -V6 -b 24 "$OutputFile" rate -u -s -b 99.9 -L $BitRate
+#    sox "$FoundFile" -C $TargetCompression -S -G -V6 -b 24 "$OutputFile" rate -v -s -b 90 $BitRate
 #    sox "$FoundFile" -C $TargetCompression -S -G -V3 -b 24 "$OutputFile" lowpass -2 21000 rate -v -s -b 90 $BitRate
+########################################################################################################################
 #    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" upsample 4 sinc -22050 -n $TAPS -L -b 0 vol 2
-#    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" upsample 4 sinc -22050 -n $TAPS -L -b 12.5 vol 2
+     sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" upsample 4 sinc -22050 -n $TAPS -L -b 12.5 vol 2
+########################################################################################################################
 #    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" upsample 4 sinc -a80 -L -21k -n $TAPS
-#    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" lowpass -2 21000 upsample 4 sinc -22050 -n $TAPS -b 0 vol 3
 #    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" upsample 2 sinc -a80 -L -21k -n $TAPS
-#    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" upsample 4 sinc -a80 -L -21k -n $TAPS
+#    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" upsample 4 sinc -a80 -L -20k -n $TAPS
 #    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" sinc -a80 -I -21.5k -t8000
-    sox "$OutputFile" -n spectrogram -o "$OutputDirectory/spectrals/${OutputBasename%.flac}.jpg"
+########################################################################################################################
+#    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" lowpass -2 21000 upsample 4 sinc -22050 -n $TAPS -b 0 vol 3
+
+
+#    sox "$OutputFile" -n spectrogram -o "$OutputDirectory/spectrals/${OutputBasename%.flac}.jpg"
   else
     mkdir -p "$OutputDirectory"
     cp "$FoundFile" "$OutputFile"
