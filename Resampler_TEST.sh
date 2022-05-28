022ee2b30b5dbfe87ce16ef61df43919c5a39da8
melk0r64 <106148952+melk0r64@users.noreply.github.com>
2022-05-28 09:51:16 +0200

    Update Resampler_TEST.sh

--- Resampler_TEST.sh
+++ Resampler_TEST.sh
@@ -26,7 +26,7 @@ do
 #    sox "$FoundFile" -C $TargetCompression -S -G -V3 -b 24 "$OutputFile" lowpass -2 21000 rate -v -s -b 90 $BitRate
 ########################################################################################################################
 #    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" upsample 4 sinc -22050 -n $TAPS -L -b 0 vol 2
-     sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" upsample 4 sinc -22050 -n $TAPS -L -b 12.5 vol 2
+#    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" upsample 4 sinc -22050 -n $TAPS -L -b 12.5 vol 2
 ########################################################################################################################
 #    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" upsample 4 sinc -a80 -L -21k -n $TAPS
 #    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" upsample 2 sinc -a80 -L -21k -n $TAPS
@@ -34,6 +34,14 @@ do
 #    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" sinc -a80 -I -21.5k -t8000
 ########################################################################################################################
 #    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" lowpass -2 21000 upsample 4 sinc -22050 -n $TAPS -b 0 vol 3
+########################################################################################################################
+#    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" upsample 4 sinc -22050 -L -b 0 -u vol 2
+#    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" upsample 4 sinc -22050 -n 4000000 -L -b 99.6 -u vol 2
+#    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" upsample 4 lowpass -1 22k sinc -22050 -n 4000000 -L -b 99.6 -u vol 2
+#    sox -S -V6 "$FoundFile" -b 24 -r $BitRate "$OutputFile" upsample 4 fir pcm_176K_256K_taps.txt sinc -22050 -n 4000000 -L -b 99.6 -u vol 2
+#    sox -S -V6 "$FoundFile" -b 24 -r $BitRate -L "$OutputFile" upsample 4 fir pcm_176K_256K_taps.txt vol 2
+#    sox -S -V6 "$FoundFile" -b 24 -r $BitRate -L "$OutputFile" upsample 4 fir pcm_176K_256K_taps.txt lowpass -1 22k sinc -22050 -n 4000000 -L -b 99.6 -u vol 2
+#    sox -S -V6 "$FoundFile" -b 24 -r $BitRate -L "$OutputFile" lowpass -1 22k upsample 4 sinc -22050 -n 4000000 -L -b 99.6 -u vol 2
 
 
 #    sox "$OutputFile" -n spectrogram -o "$OutputDirectory/spectrals/${OutputBasename%.flac}.jpg"
