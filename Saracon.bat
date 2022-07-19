7fd9d3ea6c94986e366efcd31b15d5c2de658264
melk0r64 <106148952+melk0r64@users.noreply.github.com>
2022-07-19 11:38:44 +0200

    Update Saracon.bat

--- Saracon.bat
+++ Saracon.bat
@@ -36,17 +36,17 @@ for /R "%SourcePath%" %%I in (*.flac) do (
     
 Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2p -r 44100 -d tpdf -f flac -n 16bit -b normal -g -1.0 "%%I"    
     
-rem Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2p -r 48000 -d off -f flac -n 24bit -b normal -g -1.0 "%%I"
-rem	Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2p -r 88200 -d off -f flac -n 24bit -b normal -g -1.0 "%%I"
-rem	Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2p -r 96000 -d off -f flac -n 24bit -b normal -g -1.0 "%%I"
-rem	Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2p -r 176400 -d off -f flac -n 24bit -b normal -g -1.0 "%%I"
-rem	Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2p -r 192000 -d off -f flac -n 24bit -b normal -g -1.0 "%%I"
-rem	Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2p -r 352800 -d off -f flac -n 24bit -b normal -g -1.0 "%%I"
-rem	Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2p -r 384000 -d off -f flac -n 24bit -b normal -g -1.0 "%%I"
-rem Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2p -r 44100 -d powr2 -f flac -n 16bit -b normal -g -1.0 "%%I"
-rem Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2p -r 44100 -d powr1 -f flac -n 16bit -b normal -g -1.0 "%%I"
-rem Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2p -r 44100 -d powr3 -f flac -n 16bit -b normal -g -1.0 "%%I"
-rem	Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2d -r 2822400 -d off -f dff -b normal -g -1.0 "%%I"
+  rem   Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2p -r 48000 -d off -f flac -n 24bit -b normal -g -1.0 "%%I"
+  rem	Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2p -r 88200 -d off -f flac -n 24bit -b normal -g -1.0 "%%I"
+  rem	Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2p -r 96000 -d off -f flac -n 24bit -b normal -g -1.0 "%%I"
+  rem	Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2p -r 176400 -d off -f flac -n 24bit -b normal -g -1.0 "%%I"
+  rem	Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2p -r 192000 -d off -f flac -n 24bit -b normal -g -1.0 "%%I"
+  rem	Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2p -r 352800 -d off -f flac -n 24bit -b normal -g -1.0 "%%I"
+  rem	Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2p -r 384000 -d off -f flac -n 24bit -b normal -g -1.0 "%%I"
+  rem   Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2p -r 44100 -d powr2 -f flac -n 16bit -b normal -g -1.0 "%%I"
+  rem   Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2p -r 44100 -d powr1 -f flac -n 16bit -b normal -g -1.0 "%%I"
+  rem   Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2p -r 44100 -d powr3 -f flac -n 16bit -b normal -g -1.0 "%%I"
+  rem	Saracon.exe "%%I" -t "%TargetPath%!RelativePath!" -c p2d -r 2822400 -d off -f dff -b normal -g -1.0 "%%I"
     
 )
 endlocal
